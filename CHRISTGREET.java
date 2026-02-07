677abf8a07aab5c17678d697eac1cfaee65cdf8b
jayalakshmik726-netizen <jayalakshmik726@gmail.com>
2026-02-07 15:58:37 +0530

    Add files via upload

---
+++ CHRISTGREET.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		if(a==25)
+		System.out.println("christmas");
+		else
+		System.out.println("ordinary");
+    }
+}
