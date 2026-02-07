0af6bd665d4026f0a5f3c3e91c3bc381930cace4
jayalakshmik726-netizen <jayalakshmik726@gmail.com>
2026-02-07 13:37:20 +0530

    Add files via upload

---
+++ Primitive.java
@@ -0,0 +1,10 @@
+class Primitive{
+    public static void main(String[] args){
+        String name="siri";
+        int marks=470;
+        char grade='A';
+        System.out.println(name);
+        System.out.println(marks);
+        System.out.println(grade);
+    }
+}
