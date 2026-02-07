6d7a75d7e45049fd4297f750cbfa660f050c53b4
jayalakshmik726-netizen <jayalakshmik726@gmail.com>
2026-02-07 16:08:55 +0530

    Add files via upload

---
+++ NYRES (1).java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc=new Scanner(System.in);
+	    System.out.println("Enter a number:");
+	    int a=sc.nextInt();
+	    System.out.println(31*a);
+	}
+}
