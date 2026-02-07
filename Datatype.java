387fb8f55430cbb5ee1635f864fdd9d6fc50ea88
jayalakshmik726-netizen <jayalakshmik726@gmail.com>
2026-02-07 13:36:59 +0530

    Add files via upload

---
+++ Datatype.java
@@ -0,0 +1,19 @@
+
+class Datatype{
+    public static void main(String[] args){
+        Byte b=10;
+        Short s=100;
+        int a=1000;
+        Long l=8000000000L;
+        Float f=1.04f;
+        Double d=11.22d;
+        boolean g=true;
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(a);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(g);
+    }
+}
