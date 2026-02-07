3eb9b7e1359e10e9ab436fbe09c516866db194a0
jayalakshmik726-netizen <jayalakshmik726@gmail.com>
2026-02-07 14:52:25 +0530

    Add files via upload

---
+++ Logical.java
@@ -0,0 +1,16 @@
+class Logical{
+    public static void main(String[] args){
+        int a=40;
+        int b=30;
+        if(a%2==0)
+            System.out.println("the number is even");
+        a*=5;
+            System.out.println(a);
+        if(a>b || a<b){
+            System.out.println("good");
+        }
+        else{
+            System.out.println("bad");
+        }
+    }
+}
