aaf0b65893c712ac00475353f9fec9eb1d3297e0
jayalakshmik726-netizen <jayalakshmik726@gmail.com>
2026-02-07 16:25:17 +0530

    Add files via upload

---
+++ POPCORN7.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc=new Scanner(System.in);
+	    System.out.println("Enter the number of ruppes chef has:");
+	    int a=sc.nextInt();
+	    if(a>=100){
+	        System.out.println((a-100)/50);
+	    }
+	}
+
+	}
+
